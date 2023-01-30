42dd9bf21b750575261adad2297338eb87f50e3e
dominic gasperini <dominicgasperini@gmail.com>
2023-01-30 15:14:21 -0500

    updated pinConfig with wcb i/o

--- Wheel-Control-Board/Wheel-Control-Board-Code/include/pinConfig.h
+++ Wheel-Control-Board/Wheel-Control-Board-Code/include/pinConfig.h
@@ -45,21 +45,21 @@ uint8_t deviceAddress[] = {0x1a, 0x1a, 0x1a, 0x1a, 0x1a, 0x1a};
 */
 
 
-// Sensors
-#define WHEEL_SPEED_FR_SENSOR               ADC1_GPIO32_CHANNEL
-#define WHEEL_SPEED_FL_SENSOR               ADC1_GPIO32_CHANNEL
-
-#define WHEEL_HEIGHT_FR_SENSOR              ADC1_GPIO32_CHANNEL
-#define WHEEL_HEIGHT_FL_SENSOR              ADC1_GPIO32_CHANNEL
-
-#define STEERING_WHEEL_POT                  ADC1_GPIO32_CHANNEL
-
-#define PEDAL_0_PIN                         ADC1_GPIO32_CHANNEL // use these things to config pins based on gpio #
-#define PEDAL_1_PIN                         ADC1_GPIO32_CHANNEL
-#define BRAKE_0_PIN                         ADC1_GPIO32_CHANNEL
-#define BRAKE_1_PIN                         ADC1_GPIO32_CHANNEL
+// LCD Pins
+#define LCD_MISO_PIN                        ADC1_GPIO32_CHANNEL // use these things to config pins based on gpio #
+#define LCD_MOSI_PIN                        ADC1_GPIO32_CHANNEL
+#define LCD_SCK_PIN                         ADC1_GPIO32_CHANNEL
+#define LCD_CHIP_SELECT_PIN                 ADC1_GPIO32_CHANNEL
 
 // Inputs
-#define CAN_MESSAGE_INTERRUPT_PIN           15             
+#define COAST_REGEN_KNOB                    ADC1_GPIO32_CHANNEL
+#define BRAKE_REGEN_KNOB                    ADC1_GPIO32_CHANNEL
+
+#define LCD_MODE_SELECT_BUTTON              ADC1_GPIO32_CHANNEL
+#define DRIVE_MODE_SELECT_BUTTON            ADC1_GPIO32_CHANNEL
+
+#define READY_TO_DRIVE_BUTTON               ADC1_GPIO32_CHANNEL
+#define READY_TO_DRIVE_LED                  ADC1_GPIO32_CHANNEL   
 
 // Outputs
+#define FCB_CONNECTION_STATUS_LED           ADC1_GPIO32_CHANNEL
